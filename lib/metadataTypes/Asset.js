Accenture/sfmc-devtools

commit 0c194f3f90af56e4675744ef58f34a51b956d964

#452: handle File-not-found gracefully for type asset

lib/metadataTypes/Asset.js
@@ -1403,6 +1403,9 @@ class Asset extends MetadataType {
                             subType = st;
                             filePath = [basePath, this.definition.type, st, key];
                             break;
+                        } else {
+                            // file not found
+                            return [];
                         }
                     }
                     if (await File.pathExists(File.normalizePath([...filePath, fileName]))) {

commit 56aed119afa90e69ab0215126159ff73b24cf92b

#463: fixes asset not returned by getFilesToCommit and fixes #452 issue
regression caused by 0c194f3

lib/metadataTypes/Asset.js
@@ -1403,10 +1403,11 @@ class Asset extends MetadataType {
                             subType = st;
                             filePath = [basePath, this.definition.type, st, key];
                             break;
-                        } else {
-                            // file not found
-                            return [];
                         }
+                    }
+                    if (!filePath) {
+                        // file not found
+                        return [];
                     }
                     if (await File.pathExists(File.normalizePath([...filePath, fileName]))) {
                         // #1 load json to be able to find extracted text files & binary files