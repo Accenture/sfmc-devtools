7bf0cf5470516b69416374d6879a40e0892dcedc
Jörn Berkefeld <joern.berkefeld@accenture.com>
2023-03-12 02:39:13 +0100

    #139: add deploy test for user

--- test/user.test.js
+++ test/user.test.js
@@ -36,4 +36,40 @@ describe('user', () => {
             return;
         });
     });
+    describe('Deploy ================', () => {
+        beforeEach(() => {
+            testUtils.mockSetup(true);
+        });
+        it('Should create & upsert a user', async () => {
+            // WHEN
+            await handler.deploy('testInstance/_ParentBU_', ['user']);
+            // THEN
+
+            // get results from cache
+            const result = cache.getCache();
+            assert.equal(
+                result.user ? Object.keys(result.user).length : 0,
+                2,
+                'two users expected'
+            );
+            // insert
+            assert.deepEqual(
+                await testUtils.getActualJson('testNew_user', 'user', '_ParentBU_'),
+                await testUtils.getExpectedJson('1111111', 'user', 'create'),
+                'returned metadata was not equal expected for create'
+            );
+            // update
+            assert.deepEqual(
+                await testUtils.getActualJson('testExisting_user', 'user', '_ParentBU_'),
+                await testUtils.getExpectedJson('1111111', 'user', 'update'),
+                'returned metadata was not equal expected for update'
+            );
+            assert.equal(
+                testUtils.getAPIHistoryLength(),
+                7,
+                'Unexpected number of requests made. Run testUtils.logAPIHistoryDebug() to see the requests'
+            );
+            return;
+        });
+    });
 });
