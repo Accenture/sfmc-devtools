3f51cfd0cc0c66c87ae828e63cad8a7c6f1d083d
Jörn Berkefeld <joern.berkefeld@accenture.com>
2023-04-11 23:33:28 +0200

    #782: add template test for mobileKeyword

--- test/mobileKeyword.test.js
+++ test/mobileKeyword.test.js
@@ -88,68 +88,70 @@ describe('mobileKeyword', () => {
             return;
         });
     });
-    // describe('Templating ================', () => {
-    //     it('Should create a mobileKeyword template via buildTemplate and build it', async () => {
-    //         // download first before we test buildTemplate
-    //         await handler.retrieve('testInstance/testBU', ['mobileKeyword']);
-    //         // buildTemplate
-    //         const result = await handler.buildTemplate(
-    //             'testInstance/testBU',
-    //             'mobileKeyword',
-    //             ['NTIzOjc4OjA'],
-    //             'testSourceMarket'
-    //         );
-    //         assert.equal(
-    //             !!process.exitCode,
-    //             false,
-    //             'buildTemplate should not have thrown an error'
-    //         );
+    describe('Templating ================', () => {
+        it('Should create a mobileKeyword template via buildTemplate and build it', async () => {
+            // download first before we test buildTemplate
+            await handler.retrieve('testInstance/testBU', ['mobileKeyword']);
+            // buildTemplate
+            const result = await handler.buildTemplate(
+                'testInstance/testBU',
+                'mobileKeyword',
+                ['testExisting_keyword'],
+                'testSourceMarket'
+            );
+            assert.equal(
+                !!process.exitCode,
+                false,
+                'buildTemplate should not have thrown an error'
+            );
 
-    //         assert.equal(
-    //             result.mobileKeyword ? Object.keys(result.mobileKeyword).length : 0,
-    //             1,
-    //             'only one mobileKeyword expected'
-    //         );
-    //         assert.deepEqual(
-    //             await testUtils.getActualTemplateJson('NTIzOjc4OjA', 'mobileKeyword'),
-    //             await testUtils.getExpectedJson('9999999', 'mobileKeyword', 'template'),
-    //             'returned template JSON was not equal expected'
-    //         );
-    //         expect(
-    //             file(testUtils.getActualTemplateFile('NTIzOjc4OjA', 'mobileKeyword', 'amp'))
-    //         ).to.equal(
-    //             file(testUtils.getExpectedFile('9999999', 'mobileKeyword', 'template', 'amp'))
-    //         );
+            assert.equal(
+                result.mobileKeyword ? Object.keys(result.mobileKeyword).length : 0,
+                1,
+                'only one mobileKeyword expected'
+            );
+            assert.deepEqual(
+                await testUtils.getActualTemplateJson('testExisting_keyword', 'mobileKeyword'),
+                await testUtils.getExpectedJson('9999999', 'mobileKeyword', 'template'),
+                'returned template JSON was not equal expected'
+            );
+            expect(
+                file(
+                    testUtils.getActualTemplateFile('testExisting_keyword', 'mobileKeyword', 'amp')
+                )
+            ).to.equal(
+                file(testUtils.getExpectedFile('9999999', 'mobileKeyword', 'template', 'amp'))
+            );
 
-    //         // buildDefinition
-    //         await handler.buildDefinition(
-    //             'testInstance/testBU',
-    //             'mobileKeyword',
-    //             'NTIzOjc4OjA',
-    //             'testTargetMarket'
-    //         );
-    //         assert.equal(
-    //             !!process.exitCode,
-    //             false,
-    //             'buildDefinition should not have thrown an error'
-    //         );
-    //         assert.deepEqual(
-    //             await testUtils.getActualDeployJson('NTIzOjc4OjA', 'mobileKeyword'),
-    //             await testUtils.getExpectedJson('9999999', 'mobileKeyword', 'build'),
-    //             'returned deployment JSON was not equal expected'
-    //         );
-    //         expect(
-    //             file(testUtils.getActualDeployFile('NTIzOjc4OjA', 'mobileKeyword', 'amp'))
-    //         ).to.equal(file(testUtils.getExpectedFile('9999999', 'mobileKeyword', 'build', 'amp')));
+            // buildDefinition
+            await handler.buildDefinition(
+                'testInstance/testBU',
+                'mobileKeyword',
+                'testExisting_keyword',
+                'testTargetMarket'
+            );
+            assert.equal(
+                !!process.exitCode,
+                false,
+                'buildDefinition should not have thrown an error'
+            );
+            assert.deepEqual(
+                await testUtils.getActualDeployJson('testExisting_keyword', 'mobileKeyword'),
+                await testUtils.getExpectedJson('9999999', 'mobileKeyword', 'build'),
+                'returned deployment JSON was not equal expected'
+            );
+            expect(
+                file(testUtils.getActualDeployFile('testExisting_keyword', 'mobileKeyword', 'amp'))
+            ).to.equal(file(testUtils.getExpectedFile('9999999', 'mobileKeyword', 'build', 'amp')));
 
-    //         assert.equal(
-    //             testUtils.getAPIHistoryLength(),
-    //             4,
-    //             'Unexpected number of requests made. Run testUtils.logAPIHistoryDebug() to see the requests'
-    //         );
-    //         return;
-    //     });
-    // });
+            assert.equal(
+                testUtils.getAPIHistoryLength(),
+                2,
+                'Unexpected number of requests made. Run testUtils.logAPIHistoryDebug() to see the requests'
+            );
+            return;
+        });
+    });
     describe('Delete ================', () => {
         it('Should delete the item', async () => {
             // WHEN
