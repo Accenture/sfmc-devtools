185c1b4645ca7dd32d2a2967a1c9cbb897af194c
Yulia Likhytska <Yulia.Likhytska@contracted.pmi.com>
2023-05-21 22:15:11 +0200

    730-feature added tests for execute function

--- test/type.query.test.js
+++ test/type.query.test.js
@@ -277,4 +277,22 @@ describe('type: query', () => {
             return;
         });
     });
+    describe('Execute ================', () => {
+        it('Should start executing queries', async () => {
+            const execute = await handler.execute('testInstance/testBU', ['query']);
+            assert.equal(process.exitCode, false, 'execute should not have thrown an error');
+            assert.equal(execute, true, 'queries were supposed to be executed');
+            return;
+        });
+        it('Should start executing a query', async () => {
+            const execute = await handler.execute(
+                'testInstance/testBU',
+                ['query'],
+                ['testExistingQuery']
+            );
+            assert.equal(process.exitCode, false, 'execute should not have thrown an error');
+            assert.equal(execute, true, 'query was supposed to be executed');
+            return;
+        });
+    });
 });
