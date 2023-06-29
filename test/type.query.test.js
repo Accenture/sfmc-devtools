a7ea40e49d60cb7820e792f1aed5e38406508e91
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-06-29 10:15:04 +0200

    #982: test method

--- test/type.query.test.js
+++ test/type.query.test.js
@@ -127,26 +127,26 @@ describe('type: query', () => {
         it('Should deploy and execute with --execute', async () => {
             handler.setOptions({ execute: true });
             // WHEN
-            await handler.deploy('testInstance/testBU', ['query'], 'testNew_query');
+            await handler.deploy('testInstance/testBU', ['query'], ['testExisting_query']);
             // THEN
             assert.equal(
                 process.exitCode,
                 false,
                 'deploy with --execute should not have thrown an error'
             );
-            // confirm created item
+            // confirm updated item
             assert.deepEqual(
-                await testUtils.getActualJson('testNew_query', 'query'),
-                await testUtils.getExpectedJson('9999999', 'query', 'post'),
+                await testUtils.getActualJson('testExisting_query', 'query'),
+                await testUtils.getExpectedJson('9999999', 'query', 'patch'),
                 'returned metadata was not equal expected for insert query'
             );
-            expect(file(testUtils.getActualFile('testNew_query', 'query', 'sql'))).to.equal(
-                file(testUtils.getExpectedFile('9999999', 'query', 'post', 'sql'))
+            expect(file(testUtils.getActualFile('testExisting_query', 'query', 'sql'))).to.equal(
+                file(testUtils.getExpectedFile('9999999', 'query', 'patch', 'sql'))
             );
             // check number of API calls
             assert.equal(
                 testUtils.getAPIHistoryLength(),
-                12,
+                9,
                 'Unexpected number of requests made. Run testUtils.logAPIHistoryDebug() to see the requests'
             );
             return;
