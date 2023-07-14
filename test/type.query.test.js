b0db4b076de20638a4a919342d45eedf345f4d10
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-14 16:13:52 +0200

    #38: warn users about outdated files

--- test/type.query.test.js
+++ test/type.query.test.js
@@ -276,7 +276,7 @@ describe('type: query', () => {
             // check number of API calls
             assert.equal(
                 testUtils.getAPIHistoryLength(),
-                31,
+                16,
                 'Unexpected number of requests made. Run testUtils.logAPIHistoryDebug() to see the requests'
             );
             return;
