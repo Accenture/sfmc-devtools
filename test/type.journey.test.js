861877f3057b7896e5f1e1c7a880721ef769dd65
Jörn Berkefeld <joern.berkefeld@accenture.com>
2024-05-13 22:27:33 +0200

    #62: fixed test that confirms you cannot use changeKeyValue/changeKeyField on journey

--- test/type.journey.test.js
+++ test/type.journey.test.js
@@ -167,6 +167,29 @@ describe('type: journey', () => {
         });
     });
 
+    describe('Deploy ================', () => {
+        beforeEach(() => {
+            testUtils.mockSetup(true);
+        });
+
+        it('Should NOT change the key during update with --changeKeyValue and instead fail due to missing support', async () => {
+            // WHEN
+            handler.setOptions({ changeKeyValue: 'updatedKey' });
+            await handler.deploy(
+                'testInstance/testBU',
+                ['journey'],
+                ['testExisting_journey_Quicksend']
+            );
+            // THEN
+            assert.equal(
+                process.exitCode,
+                1,
+                'deploy should have thrown an error due to lack of support'
+            );
+            return;
+        });
+    });
+
     describe('Templating ================', () => {
         it('Should create a journey template via buildTemplate and build it', async () => {
             // download first before we test buildTemplate
