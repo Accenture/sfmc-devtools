f856ba80045559c3823dbe34e0672b3ea6ab402d
Jörn Berkefeld <joern.berkefeld@accenture.com>
2024-08-31 17:00:09 +0200

    #1472: refactoring

--- @types/lib/metadataTypes/Asset.d.ts
+++ @types/lib/metadataTypes/Asset.d.ts
@@ -91,7 +91,7 @@ declare class Asset extends MetadataType {
      * @param {string} deployDir directory where deploy metadata are saved
      * @returns {Promise.<AssetMap>} keyField => metadata map but sorted to ensure dependencies are deployed in correct order
      */
-    static getUpsertOrder(metadataMap: AssetMap, deployDir: string): Promise<AssetMap>;
+    static _getUpsertOrder(metadataMap: AssetMap, deployDir: string): Promise<AssetMap>;
     /**
      * MetadataType upsert, after retrieving from target and comparing to check if create or update operation is needed.
      *
