cf8fff6ad1101da2ed298e117a7a8f82dc34312d
Jörn Berkefeld <joern.berkefeld@accenture.com>
2024-09-04 14:10:19 +0200

    #1472: also run update assets sequentially to work with key changes

--- @types/lib/metadataTypes/MetadataType.d.ts
+++ @types/lib/metadataTypes/MetadataType.d.ts
@@ -268,10 +268,10 @@ declare class MetadataType {
      *
      * @param {MetadataTypeMap} metadataMap metadata mapped by their keyField
      * @param {string} deployDir directory where deploy metadata are saved
-     * @param {boolean} [runCreatesSequentially] when a type has self-dependencies creates need to run one at a time and created keys need to be cached to ensure following creates/updates have thoses keys available
+     * @param {boolean} [runUpsertSequentially] when a type has self-dependencies creates need to run one at a time and created/changed keys need to be cached to ensure following creates/updates have thoses keys available
      * @returns {Promise.<MetadataTypeMap>} keyField => metadata map
      */
-    static upsert(metadataMap: MetadataTypeMap, deployDir: string, runCreatesSequentially?: boolean): Promise<MetadataTypeMap>;
+    static upsert(metadataMap: MetadataTypeMap, deployDir: string, runUpsertSequentially?: boolean): Promise<MetadataTypeMap>;
     /**
      * helper for {@link MetadataType.upsert}
      *
