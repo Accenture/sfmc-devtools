6d4ad736c933b074d1fd94b932f3f10d60ce4155
Jörn Berkefeld <joern.berkefeld@accenture.com>
2025-01-23 10:55:06 +0100

    #0: ensure prepare-release throws no error for "nothing to commit" scenario

---
+++ prepare-release.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import { exec } from 'node:child_process';
+
+console.log('> git status --porcelain');
+exec('git status --porcelain', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error executing git status: ${stderr}`);
+        return;
+    }
+
+    if (stdout) {
+        console.log(
+            '    Found changes:',
+            '\n     - ' + stdout.split('\n').filter(Boolean).join('\n     - ')
+        );
+        console.log('> git add .');
+        exec('git add .', (err, stdout, stderr) => {
+            if (err) {
+                console.error(`Error executing git add: ${stderr}`);
+                return;
+            }
+
+            console.log('> git commit -n -m "prepare-release changes"');
+            exec('git commit -n -m "prepare-release changes"', (err, stdout, stderr) => {
+                if (err) {
+                    console.error(`Error executing git commit: ${stderr}`);
+                    return;
+                } else {
+                    console.log('Changes committed successfully.');
+                }
+            });
+        });
+    } else {
+        console.log('No untracked files to commit.');
+    }
+});
+/* eslint-enable no-console */
